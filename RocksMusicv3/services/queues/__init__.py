a767ba2dfe4a4affdb555f114d42a47ffc71eb36
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2021-10-18 21:15:37 +0500

    Add files via upload

--- RocksMusicv3/services/queues/__init__.py
+++ RocksMusicv3/services/queues/__init__.py
@@ -1,1 +1,12 @@
+# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
+# Without Credit (Mother Fucker)
+# Rocks © @Dr_Asad_Ali © Rocks
+# Owner Asad + Harshit
 
+from RocksMusicv3.services.queues.queues import clear 
+from RocksMusicv3.services.queues.queues import get
+from RocksMusicv3.services.queues.queues import is_empty
+from RocksMusicv3.services.queues.queues import put
+from RocksMusicv3.services.queues.queues import task_done
+
+__all__ = ["clear", "get", "is_empty", "put", "task_done"]
