649a890ba6075da0ed7d16b5e348401e6704a27f
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2021-10-18 21:20:52 +0500

    Add files via upload

--- RocksMusicv3/services/callsmusic/__init__.py
+++ RocksMusicv3/services/callsmusic/__init__.py
@@ -1,1 +1,11 @@
+# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
+# Without Credit (Mother Fucker)
+# Rocks © @Dr_Asad_Ali © Rocks
+# Owner Asad + Harshit
 
+from pyrogram import Client
+
+from RocksMusicv3 import config
+
+client = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
+run = client.run
