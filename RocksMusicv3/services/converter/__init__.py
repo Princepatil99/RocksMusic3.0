798284042503d95040de554b298ed1f06d9f0fab
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2021-10-18 21:20:02 +0500

    Add files via upload

--- RocksMusicv3/services/converter/__init__.py
+++ RocksMusicv3/services/converter/__init__.py
@@ -1,1 +1,14 @@
+# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
+# Without Credit (Mother Fucker)
+# Rocks © @Dr_Asad_Ali © Rocks
+# Owner Asad + Harshit
 
+from os import listdir
+from os import mkdir
+
+if 'raw_files' not in listdir():
+    mkdir('raw_files')
+
+from RocksMusicv3.services.converter.converter import convert
+
+__all__ = ["convert"]
