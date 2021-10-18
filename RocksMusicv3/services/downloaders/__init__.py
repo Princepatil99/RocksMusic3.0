12f3c09fa89ae2361f6ff476034f908da7dc7c3e
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2021-10-18 21:17:25 +0500

    Add files via upload

--- RocksMusicv3/services/downloaders/__init__.py
+++ RocksMusicv3/services/downloaders/__init__.py
@@ -1,1 +1,8 @@
+# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
+# Without Credit (Mother Fucker)
+# Rocks © @Dr_Asad_Ali © Rocks
+# Owner Asad + Harshit
 
+from RocksMusicv3.services.downloaders import youtube
+
+__all__ = ["youtube"]
---
+++ RocksMusicv3/services/downloaders/youtube.py
@@ -0,0 +1,41 @@
+# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
+# Without Credit (Mother Fucker)
+# Rocks © @Dr_Asad_Ali © Rocks
+# Owner Asad + Harshit
+
+
+from os import path
+
+from yt_dlp import YoutubeDL
+
+from RocksMusicv3.config import DURATION_LIMIT
+from RocksMusicv3.helpers.errors import DurationLimitError
+
+ydl_opts = {
+    "format": "bestaudio/best",
+    "verbose": True,
+    "geo-bypass": True,
+    "nocheckcertificate": True,
+    "outtmpl": "downloads/%(id)s.%(ext)s",
+}
+
+ydl = YoutubeDL(ydl_opts)
+
+
+def download(url: str) -> str:
+    info = ydl.extract_info(url, False)
+    duration = round(info["duration"] / 60)
+
+    if duration > DURATION_LIMIT:
+        raise DurationLimitError(
+            f"🛑 Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, "
+            f"the provided video is {duration} minute(s)",
+        )
+    try:
+        ydl.download([url])
+    except:
+        raise DurationLimitError(
+            f"🛑 Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, "
+            f"the provided video is {duration} minute(s)",
+        )
+    return path.join("downloads", f"{info['id']}.{info['ext']}")
