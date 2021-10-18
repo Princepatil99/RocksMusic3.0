636810d5c8e67b472f58ffc279a490269840d070
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2021-10-18 21:12:32 +0500

    Add files via upload

--- RocksMusicv3/services/__init__.py
+++ RocksMusicv3/services/__init__.py
@@ -1,1 +1,15 @@
-
+# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
+# Without Credit (Mother Fucker)
+# R0ck5 © @Dr_Asad_Ali © Rocks
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# Editor Gando ... Rocks Your Dad Music Bot
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# Owner Asad + Harshit
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
