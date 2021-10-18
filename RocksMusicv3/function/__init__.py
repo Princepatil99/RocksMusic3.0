030c356bcfc80ae6834fdd3070f6e9d4bb9ca38c
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2021-10-18 21:27:45 +0500

    Add files via upload

--- RocksMusicv3/function/__init__.py
+++ RocksMusicv3/function/__init__.py
@@ -1,1 +1,11 @@
+# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
+# Without Credit (Mother Fucker)
+# Rocks © @Dr_Asad_Ali © Rocks
+# Owner Asad + Harshit
 
+
+from RocksMusicv3.function.admins import admins
+from RocksMusicv3.function.admins import get
+from RocksMusicv3.function.admins import set
+
+__all__ = ["set", "get", "admins"]
---
+++ RocksMusicv3/function/admins.py
@@ -0,0 +1,20 @@
+# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
+# Without Credit (Mother Fucker)
+# Rocks © @Dr_Asad_Ali © Rocks
+# Owner Asad + Harshit
+
+from typing import Dict
+from typing import List
+
+
+admins: Dict[int, List[int]] = {}
+
+
+def set(chat_id: int, admins_: List[int]):
+    admins[chat_id] = admins_
+
+
+def get(chat_id: int) -> List[int]:
+    if chat_id in admins:
+        return admins[chat_id]
+    return []
